e1386f280183d4eba9cad8998e569b0bd2f3106f
Lisa Moran <lisamoran737@gmail.com>
2018-05-08 10:21:00 -0400

    fixed errors with points and added a question form

--- uceslmo/platforms/browser/www/js/appActivity.js
+++ uceslmo/platforms/browser/www/js/appActivity.js
@@ -1,8 +1,15 @@
 // script holding functions to load the map and perform location tracking
+// as well as load in question points
+
+
+// and a variable that will hold the question points layer
+var questionpointslayer;
 
 //create client var
 var client;
 
+//set up mymap as a global variable outside of loadmap function
+var mymap;
 
 // code to add custom markers
 var testMarkerRed = L.AwesomeMarkers.icon({
@@ -14,11 +21,30 @@ var testMarkerPink = L.AwesomeMarkers.icon({
 	markerColor: 'pink'
 });
 
-//set up mymap as a global variable outside of loadmap function
-var mymap;
+
 
 // to run when the App starts
 loadMap()
+	
+
+// track location script modified showPosition
+function trackLocation(){
+	if(navigator.geolocation) {
+	navigator.geolocation.watchPosition(showPosition);
+	}else{
+	//document.getElementById('mapid').innerHTML="Geolocation is not supported by this browser.";
+	alert("Geolocation is not supported by this browser.");
+	}
+}
+
+// display the position of the user with a popup and add to the map
+function showPosition(position){
+	
+	L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap)
+		.bindPopup("<b>You were at "+ position.coords.longitude + " "+position.coords.latitude+"!</b>");
+	mymap.setView([position.coords.latitude, position.coords.longitude], 13);		
+}
+//document.getElementById('mapid').innerHTML="L.marker(latlng, {icon:testMarkerPink}).addTo(mymap).bindPopup("<b>"position.coords"</b>");"
 
 
 // load map function
@@ -43,40 +69,21 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 }
 
 
-// track location script modified showPosition
-function trackLocation(){
-	if(navigator.geolocation) {
-	navigator.geolocation.watchPosition(showPosition);
-	}else{
-	//document.getElementById('mapid').innerHTML="Geolocation is not supported by this browser.";
-	alert("Geolocation is not supported by this browser.");
-	}
-}
-
-// display the position of the user with a popup and add to the map
-function showPosition(position){
-	var lat = position.coords.latitude
-	var lng = position.coords.longitude
-	
-	L.marker(latlng, {icon:testMarkerPink}).addTo(mymap)
-		.bindPopup("<b>Your position was "+ position.coords.longitude + " "+position.coords.latitude+"!</b>");
-	mymap.setView([lat, lng], 13);		
-}
-//document.getElementById('mapid').innerHTML="L.marker(latlng, {icon:testMarkerPink}).addTo(mymap).bindPopup("<b>"position.coords"</b>");"
 
 // load in the question point location
-// and a variable that will hold the layer itself – we need to do this outside the function so that we can use it to remove the layer later on
-var questionpointslayer;
 
 // create the code to get the questions data using an XMLHttpRequest
 function getQuestionPoints() {
 	client = new XMLHttpRequest();
 
-//client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
-client.open('GET','http://developer.cege.ucl.ac.uk:30267/getGeoJSON/quizform/geom');
+	//client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
+	//client.open('GET','http://developer.cege.ucl.ac.uk:30267/getGeoJSON/quizform/geom'); //couldn't get this to work
+	client.open('GET','http://developer.cege.ucl.ac.uk:30267/getQuestions');
+	
 	client.onreadystatechange = questionPointsResponse; // note don't use earthquakeResponse() with brackets as that doesn't work
 	client.send();
 }
+
 // create the code to wait for the response from the data server, and process the response once it is received
 function questionPointsResponse() {
 	// this function listens out for the server to say that the data is ready - i.e. has state 4
@@ -91,21 +98,16 @@ function questionPointsResponse() {
 // convert the received data - which is text - to JSON format and add it to the map
 function loadQuestionPointslayer(questionpointsdata) {
 	// convert the text received from the server to JSON
-	var questionpointsjson = JSON.parse(questionpointsdata);
+	var questionpointsjson = JSON.parse(questionpointsdata );
 
 
 	// load the geoJSON layer
-	var questionpointslayer = L.geoJson(questionpointsjson,
-		{
-			// use point to layer to create the points
-			pointToLayer: function (feature, latlng)
-			{
-				// also include a pop-up that shows the place value of the earthquakes
-				L.marker(latlng, {icon:testMarkerPink}).bindPopup("<p>question form here</p>");
-				
-			},
-		}).addTo(mymap); //consider use of openOn rather than addto for usability
-	//mymap.fitBounds(questionpointslayer.getBounds());
+	var questionpointslayer = L.geoJson(questionpointsjson).addTo(mymap);
+		
+				// also include a pop-up
+				//L.marker(latlng, {icon:testMarkerPink}).bindPopup("<p>question form here</p>");
+				//consider use of openOn rather than addto for usability
+	mymap.fitBounds(questionpointslayer.getBounds());
 }
 
 
@@ -136,4 +138,10 @@ function processDivChange(){
 			document.getElementById('ajaxtest').innerHTML = xhr.responseText;
 			loadMap();
 	}
+}
+
+
+function checkCorrect(){
+	
+	
 }
